5416a0f3b7a9f1b2e5aff243ff1afee923a57b5b
Dhineshkumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2024-06-28 07:18:26 +0530

    Time: 37 ms (97.71%), Space: 16.6 MB (92.68%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,12 +1,7 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransomNote=sorted(ransomNote)
-        magazine=sorted(magazine)
-        
-        for i in ransomNote:
-            if i in magazine:
-                magazine[magazine.index(i)]=0
-            else:
+        for l in ransomNote:
+            if l not in magazine:
                 return False
+            magazine = magazine.replace(l, " ", 1)
         return True
-            
