1b5315e7435045cf7dc504694e8f8f582e975e88
Dhineshkumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2024-07-02 09:17:12 +0530

    Time: 236 ms (80.93%), Space: 18.1 MB (59.32%) - LeetHub

--- 1284-four-divisors/1284-four-divisors.py
+++ 1284-four-divisors/1284-four-divisors.py
@@ -1,15 +1,18 @@
 class Solution:
     def sumFourDivisors(self, nums: List[int]) -> int:
-        def sum_if_four_divisors(num: int) -> int:
-            divisor = 2
-            count, sum_of_divisors = 2, num + 1 
-            while divisor <= num // divisor:
-                if num % divisor == 0:
-                    count += 1
-                    sum_of_divisors += divisor
-                    if divisor * divisor != num:
-                        count += 1
-                        sum_of_divisors += num // divisor
-                divisor += 1
-            return sum_of_divisors if count == 4 else 0
-        return sum(sum_if_four_divisors(num) for num in nums)
+        res = 0
+        
+        for num in nums:
+            divisors = set()
+            
+            for i in range(1, int(num**0.5)+1):
+                if not num % i:
+                    divisors.add(num//i)
+                    divisors.add(i)
+                if len(divisors) > 4:
+                    break
+            
+            if len(divisors) == 4:
+                res += sum(divisors)
+        
+        return res
