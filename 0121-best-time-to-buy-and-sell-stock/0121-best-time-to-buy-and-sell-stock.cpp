95a2c632ddedeca538e460de8c36fcc9cb8c660e
Dhineshkumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2024-07-17 08:36:46 +0530

    Time: 67 ms (97.24%), Space: 96.2 MB (7.57%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int maxProfit=0;
         int minimum=prices[0];
 
-        for(int i=0;i<prices.size();i++)
+        for(int i=1;i<prices.size();i++)
         {
             int cost=prices[i]-minimum;
             maxProfit=max(maxProfit,cost);
