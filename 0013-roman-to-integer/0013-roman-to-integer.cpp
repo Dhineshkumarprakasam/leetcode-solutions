295f0dabf2c537fb5b1074386a2f4e0e716a093b
Dhinesh kumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2024-12-06 06:17:10 +0530

    Time: 5 ms (67.2%), Space: 13.5 MB (37.23%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int romanToInt(string s) {
         map<char,int> dict = {{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
-
         int ans=0;
         for(int i=0;i<s.length();i++)
         {
@@ -11,7 +10,6 @@ public:
                     ans+=dict[s[i+1]]-dict[s[i]];
                     i++;
                 }
-                
             else
                 ans+=dict[s[i]];
         }
