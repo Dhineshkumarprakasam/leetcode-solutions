b68df85dc0515c15b3e8a5440c946405dabe2ee3
Dhinesh kumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2025-07-23 19:51:09 +0530

    Time: 5 ms (35.12%), Space: 45.5 MB (66.06%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -3,11 +3,7 @@ class Solution {
         HashMap<Integer,Integer> count = new HashMap<>();
 
         for(int i : nums)
-        {
-            if(count.containsKey(i) && count.get(i)==3)
-                count.remove(i);
             count.put(i,count.getOrDefault(i,0)+1);
-        }
         
         for(var i : count.entrySet())
             if(i.getValue()==1)
