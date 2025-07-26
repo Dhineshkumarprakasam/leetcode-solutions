f921326c73bc2687a0da2be6866239993856329b
Dhinesh kumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2025-07-26 15:24:36 +0530

    Time: 5 ms (0.89%), Space: 42.3 MB (6.27%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    void preorder_left(TreeNode root,ArrayList<Integer> arr)
+    {
+        if(root==null)
+        {
+            arr.add(null);
+            return;
+        }
+
+        arr.add(root.val);  
+        preorder_left(root.left,arr);
+        preorder_left(root.right,arr);
+    }
+     void preorder_right(TreeNode root,ArrayList<Integer> arr)
+    {
+        if(root==null)
+        {
+            arr.add(null);
+            return;
+        }
+        arr.add(root.val);
+        preorder_right(root.right,arr);
+        preorder_right(root.left,arr);
+
+    }
+    public boolean isSymmetric(TreeNode root) {
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        ArrayList<Integer> arr2 = new ArrayList<>();
+        System.out.println(arr1);
+        System.out.println(arr2);
+
+        if(root==null)
+            return true;
+
+        if(root.left!=null)
+            preorder_left(root.left,arr1);
+
+        if(root.right!=null)
+            preorder_right(root.right,arr2);
+
+        if(arr1.equals(arr2))
+            return true;
+        return false;
+    }
+}
