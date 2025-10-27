2d9e9cc4a4c532d07a314d4158d5537274713cd3
Dhinesh kumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2025-10-27 15:38:57 +0530

    Time: 0 ms (100%), Space: 17.8 MB (41.74%) - LeetHub

--- 4008-restore-finishing-order/4008-restore-finishing-order.py
+++ 4008-restore-finishing-order/4008-restore-finishing-order.py
@@ -1,8 +1,8 @@
 class Solution:
     def recoverOrder(self, order: List[int], friends: List[int]) -> List[int]:
-        data = {}
-        for i,j in enumerate(order):
-            data[j]=i
-        
-        friends.sort(key=lambda x:data[x])
-        return friends
+        ans=[]
+        for i in order:
+            if i in friends:
+                ans.append(i)
+        return ans
+
