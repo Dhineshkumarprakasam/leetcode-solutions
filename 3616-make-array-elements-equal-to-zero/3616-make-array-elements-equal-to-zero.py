bc78167a35f032d6891aafb5b286af7a387b0c24
Dhinesh kumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2025-10-29 04:56:50 +0530

    Time: 37 ms (97.04%), Space: 17.5 MB (99.51%) - LeetHub

---
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countValidSelections(self, nums: List[int]) -> int:
+        n=len(nums)
+        count=0
+        left=0
+        right=sum(nums)
+
+        for i in range(n):
+            left+=nums[i]
+            right-=nums[i]
+            if nums[i]!=0:
+                continue
+            if left==right:
+                count+=2
+            if abs(left-right)==1:
+                count+=1
+        return count
+                
+
+            
