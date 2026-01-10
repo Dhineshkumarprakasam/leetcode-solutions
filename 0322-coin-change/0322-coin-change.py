c6275d4a51cd853c23af584fcc16df0ee0005723
Dhinesh kumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2026-01-10 11:36:35 +0530

    Time: 956 ms (27.97%), Space: 22.2 MB (23.08%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,10 +1,27 @@
 class Solution:
+    def helper(self,amount,coins,dp):
+        if amount==0:
+            return 0
+        
+        if amount<0:
+            return -1
+
+        if amount in dp:
+            return dp.get(amount)
+        
+        mini = float('inf')
+        for i in coins:
+            sub = self.helper(amount-i,coins,dp)
+            if sub!=-1:
+                mini = min(mini,sub+1)
+        if mini == float('inf'):
+            dp[amount]=-1
+        else:
+            dp[amount]=mini
+
+        return dp[amount]
+        
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp=[amount+1]*(amount+1)
-        dp[0]=0
-        for i in range(1,amount+1):
-            for c in coins:
-                if (i-c)>=0:
-                    dp[i]=min(dp[i],1+dp[i-c])
-        return dp[amount] if dp[amount]!=(amount+1) else -1
+        dp = dict()
+        return self.helper(amount,coins,dp)
         
