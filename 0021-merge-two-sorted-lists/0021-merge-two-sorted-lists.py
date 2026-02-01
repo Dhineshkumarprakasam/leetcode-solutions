b9aed1ecae3237fd8048c1df5a8ed35305e46e8f
Dhinesh kumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2026-02-01 10:31:51 +0530

    Time: 5 ms (1.15%), Space: 19.5 MB (16.61%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,46 +5,21 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-
-        ans=None
-        tail=None
-        while(list1 is not None and list2 is not None):
-            if list1.val<list2.val:
-                if ans is None:
-                    ans = ListNode(list1.val)
-                    tail=ans
-                else:
-                    tail.next=ListNode(list1.val)
-                    tail=tail.next
+        head = ListNode()
+        tail=head
+        while(list1  is not None and list2 is not None):
+            if list1.val <= list2.val:
+                tail.next=list1
                 list1=list1.next
-            
             else:
-                if ans is None:
-                    ans = ListNode(list2.val)
-                    tail=ans
-                else:
-                    tail.next=ListNode(list2.val)
-                    tail=tail.next
+                tail.next=list2
                 list2=list2.next
+            tail=tail.next
         
-        while list1 is not None:
-            if ans is None:
-                ans = ListNode(list1.val)
-                tail=ans
-            else:
-                tail.next=ListNode(list1.val)
-                tail=tail.next
-            list1=list1.next
-        
-        while list2 is not None:
-            if ans is None:
-                ans = ListNode(list2.val)
-                tail=ans
-            else:
-                tail.next=ListNode(list2.val)
-                tail=tail.next
-            list2=list2.next
-        
-        return ans
-            
+        if list1 is None:
+            tail.next=list2
+        else:
+            tail.next=list1
+        return head.next
+
 
