db5472d54abc39cee2809d51d209cc28bc9574a8
Dhinesh kumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2026-02-01 14:32:47 +0530

    Time: 3 ms (62.26%), Space: 20.4 MB (34.61%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,8 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n=len(nums)
-        need=(n*(n+1))/2
-        actual=0
-        for i in nums:
-            actual+=i
-        return int(need-actual)
+        total=sum(nums)
+        large = len(nums)
+        return ((large*(large+1))//2) - total
+    
