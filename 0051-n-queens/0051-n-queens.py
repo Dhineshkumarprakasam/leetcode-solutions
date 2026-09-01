341fd296b7a322b21d93e47a583406db8a67481c
Dhinesh kumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2026-09-02 05:25:18 +0530

    Time: 39 ms (11.19%), Space: 19.7 MB (70.65%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,57 +1,60 @@
 class Solution:
+
+    def find_ans(self,board,ans):
+        temp=[]
+        for i in board:
+            row = ""
+            for j in i:
+                if j==True:
+                    row+="Q"
+                else:
+                    row+="."
+            temp.append(row)
+        ans.append(temp)
+
     def isSafe(self,board,r,c):
         tr,tc=r,c
-        while tc>=0 and tr>=0:
-            if board[tr][tc]:
+        while(tr>=0 and tc>=0):
+            if(board[tr][tc]):
                 return False
-            tc-=1
             tr-=1
+            tc-=1
         
         tr,tc=r,c
-        while tc<len(board)  and tr>=0:
+        while(tc<len(board) and tr>=0):
             if board[tr][tc]:
                 return False
-            tc+=1
             tr-=1
-
+            tc+=1
+        
         tr=r
-        while tr>=0:
+        while(tr>=0):
             if board[tr][c]:
                 return False
             tr-=1
         
         return True
 
-    def find_ans(self,board,ans):
-        temp=[]
-        for i in range(len(board)):
-            row = ""
-            for j in range(len(board)):
-                if board[i][j]:
-                    row+="Q"
-                else:
-                    row+="."
-            temp.append(row)
-        ans.append(temp)
-    
     def nq(self,board,n,r,ans):
-        if r==n:
+        if(r==n):
             self.find_ans(board,ans)
             return
-
+        
         for c in range(n):
             if self.isSafe(board,r,c):
                 board[r][c]=True
                 self.nq(board,n,r+1,ans)
                 board[r][c]=False
-            
+
     def solveNQueens(self, n: int) -> List[List[str]]:
         board=[]
         ans=[]
+
         for i in range(n):
-            temp=[]
+            temp = []
             for j in range(n):
                 temp.append(False)
             board.append(temp)
+        
         self.nq(board,n,0,ans)
         return ans
